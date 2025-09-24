037c85a311f5fb9e57d19ff12705aca3a3fce932
Flip <felipecamposborgesinf@gmail.com>
2025-09-23 22:39:56 -0300

    Veiculo.java

---
+++ Veiculo.java
@@ -0,0 +1,27 @@
+package Veiculo;
+
+// Criação classe abstrata Veiculo 
+public abstract class Veiculo {
+    public String marca;
+    public String modelo;
+    private int ano;
+
+    // Construtor opcional
+    public Veiculo(String marca, String modelo, int ano) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+
+    // Métodos de acesso (getter e setter) para ano
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    // Método abstrato
+    public abstract String informacoesVeiculo();
+}
